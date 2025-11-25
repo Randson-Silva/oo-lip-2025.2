8e47521fd643f8332f2f76754dc0e3a78abedfb8
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2025-11-25 08:47:06 -0300

    chore: project set up

---
+++ src/main/java/com/lip/lip/LipApplication.java
@@ -0,0 +1,13 @@
+package com.lip.lip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lip/lip/LipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lip.lip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
