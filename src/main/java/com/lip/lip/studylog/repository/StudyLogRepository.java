ca122b5e8af6cfa19181e6631f8913229e145a5b
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2026-01-06 10:47:14 -0300

    feat: add query methods to count total studies by user ID and retrieve studies per discipline

--- src/main/java/com/lip/lip/studylog/repository/StudyLogRepository.java
+++ src/main/java/com/lip/lip/studylog/repository/StudyLogRepository.java
@@ -1,9 +1,20 @@
 package com.lip.lip.studylog.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.lip.lip.disicipline.dtos.response.DisciplinePerformanceDto;
 import com.lip.lip.studylog.entity.StudyLog;
 
 public interface StudyLogRepository extends JpaRepository<StudyLog, Long>{
-    
+    @Query("SELECT COUNT(s) FROM StudyLog s WHERE s.user.id = :userId")
+    Long countTotalStudiesByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT new com.lip.lip.disicipline.dtos.response.DisciplinePerformanceDto(d.name, COUNT(s), 0L) " +
+           "FROM StudyLog s JOIN s.discipline d " +
+           "WHERE s.user.id = :userId GROUP BY d.name")
+    List<DisciplinePerformanceDto> getStudiesPerDiscipline(@Param("userId") Long userId);
 }
