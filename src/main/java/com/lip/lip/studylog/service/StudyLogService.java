06a7dda1de9c795af9147d47879261598802116b
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2026-01-07 12:25:46 -0300

    refactor: simplify createStudy method and update revision scheduling logic

--- src/main/java/com/lip/lip/studylog/service/StudyLogService.java
+++ src/main/java/com/lip/lip/studylog/service/StudyLogService.java
@@ -1,15 +1,16 @@
 package com.lip.lip.studylog.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lip.lip.revision.entity.Revision;
 import com.lip.lip.revision.repository.RevisionRepository;
 import com.lip.lip.studylog.entity.StudyLog;
 import com.lip.lip.studylog.repository.StudyLogRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -20,20 +21,21 @@ public class StudyLogService {
     private final RevisionRepository revisionRepository;
 
     @Transactional
-    public StudyLog createStudyAndScheduleRevisions(StudyLog studyLog) {
-        StudyLog savedLog = studyLogRepository.save(studyLog);
+    public StudyLog createStudy(StudyLog log) {
+        log.setStudyDate(LocalDate.now());
+        StudyLog savedLog = studyLogRepository.save(log);
 
-        List<Integer> revisionDays = List.of(1, 7, 14);
+        List<Integer> revisionGaps = List.of(1, 7, 14);
 
-        revisionDays.forEach(daysToAdd -> {
+        for (Integer days : revisionGaps) {
             Revision revision = new Revision();
-            revision.setScheduledDate(savedLog.getStudyDate().plusDays(daysToAdd));
+            revision.setScheduledDate(savedLog.getStudyDate().plusDays(days));
             revision.setStudyLog(savedLog);
             revision.setUser(savedLog.getUser());
             revision.setCompleted(false);
             
             revisionRepository.save(revision);
-        });
+        }
 
         return savedLog;
     }
