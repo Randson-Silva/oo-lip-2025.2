b6cfa8f7132829351f48f3bc8cfd2c30f9a2ee9e
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2026-01-07 12:26:07 -0300

    feat: add ScheduleItemRepository and ScheduleService for schedule management

---
+++ src/main/java/com/lip/lip/scheduleItem/repository/ScheduleItemRepository.java
@@ -0,0 +1,9 @@
+package com.lip.lip.scheduleItem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lip.lip.scheduleItem.entity.ScheduleItem;
+
+public interface ScheduleItemRepository extends JpaRepository<ScheduleItem, Long>{
+
+}
